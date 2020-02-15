30e01b9879786fd7e970bfeb63f8d1a207d75888
Çağatay Yücelen <cagatayyucelen@gmail.com>
2020-02-15 17:29:00 +0300

    simple usage example added

---
+++ example/simple/auth.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+)
+
+type AuthClient struct {
+	authURL string
+}
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Token struct {
+	Token string `json:"token"`
+}
+
+var NotAuthorizedError = errors.New("User not authorized")
+var InternalAuthServerError = errors.New("Something went wrong in those lands")
+
+func (a *AuthClient) Login(credentials Credentials) (Token, error) {
+	credentialsJSON, _ := json.Marshal(credentials)
+
+	req, _ := http.NewRequest(http.MethodPost, a.authURL+"/login", bytes.NewReader(credentialsJSON))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("X-If-You-Read-This", "send-a-hadouken-back")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return Token{}, err
+	}
+
+	if res.StatusCode == http.StatusOK {
+		body, _ := ioutil.ReadAll(res.Body)
+
+		token := Token{}
+		json.Unmarshal(body, &token)
+
+		return token, nil
+	}
+
+	if res.StatusCode == http.StatusUnauthorized {
+		return Token{}, NotAuthorizedError
+	}
+
+	return Token{}, InternalAuthServerError
+}
---
+++ example/simple/auth_test.go
@@ -0,0 +1,61 @@
+package auth
+
+import (
+	"aduket"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogin(t *testing.T) {
+	// Create a new aduket server to respond on /login with given ResponseRules
+	// and to record incoming request
+	server, requestRecorder := aduket.NewServer(
+		http.MethodPost, "/login",
+		aduket.StatusCode(http.StatusOK),
+		aduket.JSONBody(Token{"12345"}),
+	)
+	defer server.Close()
+
+	auth := AuthClient{authURL: server.URL}
+
+	credentials := Credentials{Username: "streetbyters", Password: "aduket"}
+	actualToken, err := auth.Login(credentials)
+	assert.Nil(t, err)
+
+	// Assert if request sent with correct body
+	requestRecorder.AssertJSONBodyEqual(t, credentials)
+	// Assert if request sent with correct headers
+	requestRecorder.AssertHeaderEqual(t, http.Header{"X-If-You-Read-This": []string{"send-a-hadouken-back"}})
+
+	assert.Equal(t, Token{"12345"}, actualToken)
+}
+
+func TestUnauthorizedLogin(t *testing.T) {
+	// Create a new aduket server to response with 401 on /login
+	server, _ := aduket.NewServer(
+		http.MethodPost, "/login",
+		aduket.StatusCode(http.StatusUnauthorized),
+	)
+	defer server.Close()
+
+	auth := AuthClient{authURL: server.URL}
+
+	_, err := auth.Login(Credentials{Username: "root", Password: "toor"})
+	assert.Equal(t, err, NotAuthorizedError)
+}
+
+func TestAuthServerError(t *testing.T) {
+	// Create a new aduket server to response with 500 on /login
+	server, _ := aduket.NewServer(
+		http.MethodPost, "/login",
+		aduket.StatusCode(http.StatusInternalServerError),
+	)
+	defer server.Close()
+
+	auth := AuthClient{authURL: server.URL}
+
+	_, err := auth.Login(Credentials{Username: "streetbyters", Password: "aduket"})
+	assert.Equal(t, err, InternalAuthServerError)
+}
